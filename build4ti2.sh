1ac6e07d6a93415ea0402eb91aed6b5043e4a5ff
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2023-04-22 14:28:53 +0200

    going for sudo make install + standard folders

--- build4ti2.sh
+++ build4ti2.sh
@@ -10,18 +10,19 @@ echo "building libGMP"
 wget --progress=dot:mega https://gmplib.org/download/gmp/gmp-6.2.1.tar.bz2 ; 
 tar xjf gmp-6.2.1.tar.bz2 ; 
 cd gmp-6.2.1 ; 
-./configure --enable-cxx --enable-fat --prefix=$IDIR  --build=westmere-pc-linux-gnu --disable-shared
+# --prefix=$IDIR
+./configure --enable-cxx --enable-fat --build=westmere-pc-linux-gnu --disable-shared
 make -j 
-make install 
+sudo make install 
 cd ..     
 
 echo "building glpk"
 wget --progress=dot:mega http://ftp.gnu.org/gnu/glpk/glpk-5.0.tar.gz
 tar xzf glpk-5.0.tar.gz 
 cd glpk-5.0/
-./configure CFLAGS="-I$IDIR/include" LDFLAGS="-L$IDIR/lib" --prefix=$IDIR
+./configure CFLAGS="-I$IDIR/include" LDFLAGS="-L$IDIR/lib" 
 make -j
-make install
+sudo make install
 cd ..
 
 ls -laR $IDIR
@@ -32,10 +33,11 @@ tar xzf 4ti2-1.6.9.tar.gz
 cd 4ti2-1.6.9/
 autoreconf -vfi
 
-for i in $(find -name Makefile.am -print) ; do sed -i 's/AM_CXXFLAGS =/AM_CXXFLAGS = $(CXXFLAGS)/g' $i ; done
+# for i in $(find -name Makefile.am -print) ; do sed -i 's/AM_CXXFLAGS =/AM_CXXFLAGS = $(CXXFLAGS)/g' $i ; done
 
 echo "4ti2int64_LDFLAGS=-all-static -static-libgcc -static-libstdc++ \$(LDFLAGS)" >> src/groebner/Makefile.am ;  
-./configure CFLAGS="-I$IDIR/include" CXXFLAGS="-I$IDIR/include" LDFLAGS="-I$IDIR/include -L$IDIR/lib" --prefix=$IDIR
+./configure 
+# CFLAGS="-I$IDIR/include" CXXFLAGS="-I$IDIR/include" LDFLAGS="-I$IDIR/include -L$IDIR/lib" --prefix=$IDIR
 make -j
 rm src/groebner/4ti2int64
 make
