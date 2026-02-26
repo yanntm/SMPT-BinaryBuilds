a795067e77edc5dc12ff3fa4ee0732f42efc564e
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2026-02-26 18:39:09 +0100

    revert gmp version

--- build4ti2.sh
+++ build4ti2.sh
@@ -1,36 +1,33 @@
-#! /bin/bash
-
+#!/bin/bash
 set -x
 
-mkdir -p usr/
 mkdir -p usr/local/
 export IDIR=$PWD/usr/local/
 
 echo "building libGMP"
-wget --progress=dot:mega https://gmplib.org/download/gmp/gmp-6.3.0.tar.bz2 ; 
-tar xjf gmp-6.3.0.tar.bz2 ; 
-cd gmp-6.3.0 ; 
-# --prefix=$IDIR
-./configure --enable-cxx --enable-fat --build=westmere-pc-linux-gnu --disable-shared
-make -j 
-sudo make install 
-cd ..     
+wget --progress=dot:mega https://gmplib.org/download/gmp/gmp-6.2.1.tar.bz2
+tar xjf gmp-6.2.1.tar.bz2
+cd gmp-6.2.1
+./configure --enable-cxx --enable-fat --disable-shared --prefix="$IDIR" CC="gcc -std=gnu17"
+# --build=westmere-pc-linux-gnu : required in MCC, but obsolete.
+make -j
+make install          # no sudo needed any more
+cd ..
 
 echo "building glpk"
 wget --progress=dot:mega http://ftp.gnu.org/gnu/glpk/glpk-5.0.tar.gz
-tar xzf glpk-5.0.tar.gz 
+tar xzf glpk-5.0.tar.gz
 cd glpk-5.0/
-./configure CFLAGS="-I$IDIR/include" LDFLAGS="-L$IDIR/lib" 
+./configure --prefix="$IDIR" CFLAGS="-I$IDIR/include" LDFLAGS="-L$IDIR/lib"
 make -j
-sudo make install
+make install          # no sudo needed any more
 cd ..
-
-ls -laR $IDIR
 
 echo "Building 4ti2"
 wget --progress=dot:mega https://github.com/4ti2/4ti2/releases/download/Release_1_6_14/4ti2-1.6.14.tar.gz
 tar xzf 4ti2-1.6.14.tar.gz
 cd 4ti2-1.6.14/
+
 autoreconf -vfi
 
 # for i in $(find -name Makefile.am -print) ; do sed -i 's/AM_CXXFLAGS =/AM_CXXFLAGS = $(CXXFLAGS)/g' $i ; done
@@ -43,12 +40,14 @@ make -j
 rm src/groebner/4ti2int64
 rm src/zsolve/zsolve
 make
+
 mkdir -p ../website
 cp src/groebner/4ti2int64 ../website/
 cp src/groebner/qsolve ../website/
 cp src/groebner/zbasis ../website/
 cp src/zsolve/zsolve ../website/
 cp src/zsolve/hilbert ../website/
+
 strip -s ../website/4ti2int64
 strip -s ../website/zsolve
 cd ..
